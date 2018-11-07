c177795b6b3e2cfba35e57216b9d305217a713a2
gbellini90 <gbellini90@gmail.com>
2018-11-07 13:59:03 -0500

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
+
+
+
+
+-- prior example for creating a cats table
+-- CREATE TABLE cats (
+--     id INTEGER PRIMARY KEY,
+--     name TEXT,
+--     age INTEGER,
+--     breed TEXT,
+--     owner_id INTEGER
+-- );
---
+++ lib/insert.sql
@@ -0,0 +1,39 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Mr. Chocolate', 3, 'male', 'brown', 'nice', true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Rowdy', 6, 'male', 'brown', 'rowdy', true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Tabitha', 4, 'female', 'black', 'nice', true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Sergeant Brown', 7, 'female', 'white', "mean", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Melissa', 6, 'female', 'brown', 'nice', false);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Grinch', 2, 'male', 'black', 'nice', true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Wendy', 1, 'female', 'white', 'nice', false);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (Null, 1, 'female', 'white', 'nice', false);
+
+-- id INTEGER PRIMARY KEY,
+-- name TEXT,
+-- age INTEGER,
+-- gender TEXT,
+-- color TEXT,
+-- temperament TEXT,
+-- alive BOOLEAN
+
+
+
+-- INSERT INTO cats
+-- (name, age, breed, owner_id)
+-- VALUES ("Maru", 3 , "Scottish Fold", 1);
+
+-- SELECT artists.name, albums.title FROM artists INNER JOIN albums ON artists.Artistid=albums.Artistid
