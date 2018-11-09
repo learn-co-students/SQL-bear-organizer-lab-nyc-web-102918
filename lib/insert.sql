818119e5f81c524fc9fd23919be329c05cee9b9c
Hamilton Hudson <ehamiltonhudson@gmail.com>
2018-11-08 20:24:37 -0500

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "male", "brown", "docile", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "male", "black", "rambunctious", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "female", "panda", "playful", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 20, "male", "brown", "grumpy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 14, "female", "brown", "standoffish", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 9, "male", "black", "aggressive", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 24, "female", "polar", "loving", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 28, "male", "brown", "homicidal", "false")
